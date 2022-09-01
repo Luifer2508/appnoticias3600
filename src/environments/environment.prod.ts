6896a44a8fcaeb4700b9655e9dc3354e9bebda28
Luis Corrales <Luiscorrales2508@gmail.com>
2022-08-31 23:46:49 -0400

    categorias y links

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiKey:'e6fe93c179924ed8b73e585fd68e364e'
+  apiKey:'e6fe93c179924ed8b73e585fd68e364e',
+  url:'https://newsapi.org/v2',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,8 @@
 
 export const environment = {
   production: false,
-  apiKey:'e6fe93c179924ed8b73e585fd68e364e'
+  apiKey:'e6fe93c179924ed8b73e585fd68e364e',
+  url: 'https://newsapi.org/v2'
 };
 
 /*
